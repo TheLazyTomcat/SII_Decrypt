29ae2d3864c813b934ef944f1708484215ca776c
František Milt <frantisek.milt@gmail.com>
2016-02-05 16:27:17 +0100

    Added licensing notice into *.dpr and *.lpr files

--- Tester/Delphi/SII_Decrypt_Tester.dpr
+++ Tester/Delphi/SII_Decrypt_Tester.dpr
@@ -1,9 +1,15 @@
+{-------------------------------------------------------------------------------
+
+  This Source Code Form is subject to the terms of the Mozilla Public
+  License, v. 2.0. If a copy of the MPL was not distributed with this
+  file, You can obtain one at http://mozilla.org/MPL/2.0/.
+
+-------------------------------------------------------------------------------}
 program SII_Decrypt_Tester;
 
 {$APPTYPE CONSOLE}
 
 uses
-  SysUtils,
   SII_DecryptLib in '..\..\Headers\SII_DecryptLib.pas';
 
 begin
